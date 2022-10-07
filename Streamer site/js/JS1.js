86ed48868e5f8572f5ae2f5a6b7d76dca879454c
KSolis-dev <115271585+KSolis-dev@users.noreply.github.com>
2022-10-07 13:10:28 -0400

    Add files via upload

---
+++ Streamer site/js/JS1.js
@@ -0,0 +1,11 @@
+function updateCheckbox(){
+	if (follow.checked) {
+		document.getElementById('followme').innerHTML ="Thank you! Following!";
+		alert("You are not really following an imaginery streamer!  Thanks for trying out my function -KSolis");
+	}
+	else
+		document.getElementById('followme').innerHTML="Follow?";
+}
+
+
+
